0efc7deb565e655a8aa694dd25573056d6682b6b
Jor_Poon <jormpoon@gmail.com>
2019-07-11 12:10:21 -0400

    day 3 mvp completed

--- client_mining_p/blockchain.py
+++ client_mining_p/blockchain.py
@@ -142,24 +142,30 @@ blockchain = Blockchain()
 
 @app.route('/mine', methods=['POST'])
 def mine():
+    values = request.get_json()
+
+    # Check that the required fields are in the POST'ed data
+    required = ['proof']
+    if not all(k in values for k in required):
+        return 'Missing Values', 400
     # We run the proof of work algorithm to get the next proof...
     last_block = blockchain.last_block
     last_proof = last_block['proof']
     # proof = blockchain.proof_of_work(last_proof)
-    if blockchain.valid_proof(last_proof, request.form) is True:
+    if blockchain.valid_proof(last_proof, values['proof']) is True:
         
 
     # We must receive a reward for finding the proof.
     # The sender is "0" to signify that this node has mine a new coin
         blockchain.new_transaction(
             sender="0",
-            recipient=node_identifier,
+            recipient= "Jor",
             amount=1,
         )
 
         # Forge the new BLock by adding it to the chain
         previous_hash = blockchain.hash(last_block)
-        block = blockchain.new_block(request.form, previous_hash)
+        block = blockchain.new_block(values['proof'], previous_hash)
 
         response = {
             'message': "New Block Forged",
@@ -168,9 +174,9 @@ def mine():
             'proof': block['proof'],
             'previous_hash': block['previous_hash'],
         }
-        return jsonify(response), 200
+        return jsonify(response), 201
     else:
-        return "Keep Mining!!!"
+        return "Keep Mining!!!", 400
 
 
 @app.route('/transactions/new', methods=['POST'])
@@ -201,7 +207,7 @@ def full_chain():
 
 
 # -------------------------------------------------
-@app.route('/last-proof', methods=['GET'])
+@app.route('/proof', methods=['GET'])
 def prior_proof():
     response = {
         'last_proof': blockchain.chain[-1]['proof']
--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -1,11 +1,35 @@
 import hashlib
 import requests
+import json
 
 import sys
 
 
 # TODO: Implement functionality to search for a proof 
 
+url = "http://localhost:5000/"
+def valid_proof(last_proof, proof):
+    """
+    Validates the Proof:  Does hash(last_proof, proof) contain 4
+    leading zeroes?
+    """
+    guess = f'{last_proof}{proof}'.encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+    return guess_hash[:6] == "000000"
+
+def proof_of_work(last_proof):
+    """
+    Simple Proof of Work Algorithm
+    - Find a number p' such that hash(pp') contains 4 leading
+    zeroes, where p is the previous p'
+    - p is the previous proof, and p' is the new proof
+    """
+
+    proof = 0
+    while valid_proof(last_proof, proof) is False:
+        proof += 1
+
+    return proof
 
 if __name__ == '__main__':
     # What node are we interacting with?
@@ -18,8 +42,24 @@ if __name__ == '__main__':
     # Run forever until interrupted
     while True:
         # TODO: Get the last proof from the server and look for a new one
+        response = requests.get(f"{url}proof")
+        data = response.json()
+        request_proof = data['last_proof']
+        print(request_proof)
+        # print(proof_of_work(request_proof), "hello")
+        new_proof = proof_of_work(request_proof)
+        # data = {"proof": new_proof}
+        print(new_proof)
         # TODO: When found, POST it to the server {"proof": new_proof}
+        post_proof = requests.post(f"{url}mine", json={"proof": new_proof})
+        print(post_proof.json())
         # TODO: If the server responds with 'New Block Forged'
+        if post_proof.status_code == 201:
+            coins_mined += 1
+            print(coins_mined)
+        else:
+            print(post_proof.json())
+
         # add 1 to the number of coins mined and print it.  Otherwise,
         # print the message from the server.
-        pass
+        
